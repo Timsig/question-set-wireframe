9f59bad7cd33d21000418e416321aa7d9fbbfa91
Tim Signore <timsignore@gmail.com>
2019-04-15 21:19:08 +0100

    Revealer scrolls to top

--- src/components/q_revealer.js
+++ src/components/q_revealer.js
@@ -9,51 +9,67 @@ import Qselect from "./q_select"
 // )
 
 class Qrevealer extends Component {
-  state = {
-    height: 0,
-    inputValue: ""
+  constructor(props) {
+    super(props)
+    this.state = {
+      height: 0,
+      inputValue: "",
+    }
+    this.myRef = React.createRef()
   }
 
-  updateInputValue = (evt) => {
+  updateInputValue = evt => {
     this.setState({
-      inputValue: evt.target.value
+      inputValue: evt.target.value,
     })
   }
 
   open = () => {
-    if (/^([a-zA-Z]){1}([0-9][0-9]|[0-9]|[a-zA-Z][0-9][a-zA-Z]|[a-zA-Z][0-9][0-9]|[a-zA-Z][0-9]){1}([ ]?)([0-9][a-zA-z][a-zA-z]){1}$/.test(this.state.inputValue)){
+    if (
+      /^([a-zA-Z]){1}([0-9][0-9]|[0-9]|[a-zA-Z][0-9][a-zA-Z]|[a-zA-Z][0-9][0-9]|[a-zA-Z][0-9]){1}([ ]?)([0-9][a-zA-z][a-zA-z]){1}$/.test(
+        this.state.inputValue
+      )
+    ) {
       this.setState({
         height: "auto",
-        error: false
+        error: false,
       })
-    }else{
+      window.scrollTo({
+        left: 0,
+        top: this.myRef.current.offsetTop,
+        behavior: "smooth",
+      })
+    } else {
       this.setState({
         error: true,
-        height: 0
+        height: 0,
       })
     }
   }
 
-  
-
   render() {
-    const errorMessage = <div className="errorMessage">Please enter a valid UK postcode</div>
+    const errorMessage = (
+      <div className="errorMessage">Please enter a valid UK postcode</div>
+    )
     return (
-      <div className="qwrap qwrap-revealer">
+      <div className="qwrap qwrap-revealer" ref={this.myRef}>
         <label htmlFor={this.props.id}>{this.props.question}</label>
         <div className="inline-form-field">
-          <input id={this.props.id} type="text" value={this.state.inputValue} onChange={this.updateInputValue} size="9"/>
+          <input
+            id={this.props.id}
+            type="text"
+            value={this.state.inputValue}
+            onChange={this.updateInputValue}
+            size="9"
+          />
           <button onClick={this.open}>Submit</button>
         </div>
-        {this.state.error ? errorMessage : ""} 
-        <AnimateHeight
-          duration={500}
-          height={this.state.height} 
-        >
+        {this.state.error ? errorMessage : ""}
+        <AnimateHeight duration={500} height={this.state.height}>
           <Qselect
             id={this.props.selectId}
             question={this.props.selectQ}
-            options={this.props.selectOptions}  
+            options={this.props.selectOptions}
           />
         </AnimateHeight>
       </div>
